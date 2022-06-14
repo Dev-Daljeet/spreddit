6a8f3bc16cfbaed799945ca2532290c0634fa5db
Dev-Daljeet <sin10510@sheridancollege.ca>
2022-06-14 14:12:48 -0400

    Add model classes

---
+++ src/main/java/com/devdaljeet/spreddit/model/NotificationEmail.java
@@ -0,0 +1,14 @@
+package com.devdaljeet.spreddit.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationEmail {
+    private String subject;
+    private String recipient;
+    private String body;
+}
