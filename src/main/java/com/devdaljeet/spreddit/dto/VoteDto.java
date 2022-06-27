541adec70ef724e490468d6940219d91259f417a
Dev-Daljeet <sin10510@sheridancollege.ca>
2022-06-27 16:39:35 -0400

    Add dto and mapper classes

---
+++ src/main/java/com/devdaljeet/spreddit/dto/VoteDto.java
@@ -0,0 +1,15 @@
+package com.devdaljeet.spreddit.dto;
+
+import com.devdaljeet.spreddit.model.VoteType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
