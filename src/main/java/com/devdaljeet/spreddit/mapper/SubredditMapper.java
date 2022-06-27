6a984e9d233551f37d7ac668e0693996763b0885
Dev-Daljeet <sin10510@sheridancollege.ca>
2022-06-27 16:40:27 -0400

    Add security and controller classes

---
+++ src/main/java/com/devdaljeet/spreddit/mapper/SubredditMapper.java
@@ -0,0 +1,26 @@
+package com.devdaljeet.spreddit.mapper;
+
+import java.util.List;
+
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.devdaljeet.spreddit.dto.SubredditDto;
+import com.devdaljeet.spreddit.model.Post;
+import com.devdaljeet.spreddit.model.Subreddit;
+
+@Mapper(componentModel = "spring")
+public interface SubredditMapper {
+
+    @Mapping(target = "numberOfPosts", expression = "java(mapPosts(subreddit.getPosts()))")
+    SubredditDto mapSubredditToDto(Subreddit subreddit);
+
+    default Integer mapPosts(List<Post> numberOfPosts) {
+        return numberOfPosts.size();
+    }
+
+    @InheritInverseConfiguration
+    @Mapping(target = "posts", ignore = true)
+    Subreddit mapDtoToSubreddit(SubredditDto subredditDto);
+}
